87442f405dc8a6b77948fc0db4f3c730ebde26a1
samarthapa <thapasamar48@gmail.com>
2024-06-10 09:40:22 +0545

    auto play feature completed

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,88 +1,99 @@
-let result = '';
+let result = "";
 
-let score = JSON.parse(localStorage.getItem('score'));
-if(!score){
-  score ={
-    wins:0,
-    losses:0,
-    ties:0
-  }
+let score = JSON.parse(localStorage.getItem("score"));
+if (!score) {
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+  };
 }
 updateScoreElement();
 
-function createSignals(yourChoice){
-const num = Math.random();
-let computerMove = '';
-  if(num>=0 && num<1/3){
-    computerMove = 'Rock';
-    if(yourChoice==='Rock'){
-    result = 'It\'s a Tie.';
-   }
-    else if(yourChoice==='Paper'){
-    result = 'You win.';
-    }
-    else{
-    result = 'You lose.';
-    }
+function pickComputerMove() {
+  const num = Math.random();
+  let computersMove = "";
+  if (num >= 0 && num < 1 / 3) {
+    computersMove = "Rock";
+  } else if (num >= 1 / 3 && num < 2 / 3) {
+    computersMove = "Paper";
+  } else if (num >= 2 / 3 && num <= 1) {
+    computersMove = "Scissors";
   }
-else if(num>=1/3 && num<2/3){
-  computerMove = 'Paper';
-  if(yourChoice==='Paper'){
-    result = 'It\'s a Tie.';
-   }
-    else if(yourChoice==='Rock'){
-      result = 'You lose.';
-    }
-    else{
-      result = 'You win.';
-    }
- } 
-else if(num>=2/3 && num <=1){
-  computerMove = 'Scissors';
-  if(yourChoice==='Scissors'){
-    result = 'It\'s a Tie.';
-   }
-    else if(yourChoice==='Rock'){
-      result = 'You win.';
-    }
-    else{
-      result = 'You lose.';
-    }
-}
-if(result === 'You win.'){
-  score.wins++;
-}
-else if(result === 'You lose.'){
-  score.losses++;
-}
-else{
-  score.ties++;
+  return computersMove;
 }
 
-localStorage.setItem('score',JSON.stringify(score));
-document.querySelector('.js-players-move').innerHTML= `you
+function createSignals(yourChoice) {
+  let computerMove = pickComputerMove();
+  if (computerMove === "Rock") {
+    if (yourChoice === "Rock") {
+      result = "It's a Tie.";
+    } else if (yourChoice === "Paper") {
+      result = "You win.";
+    } else {
+      result = "You lose.";
+    }
+  } else if (computerMove === "Paper") {
+    if (yourChoice === "Paper") {
+      result = "It's a Tie.";
+    } else if (yourChoice === "Rock") {
+      result = "You lose.";
+    } else {
+      result = "You win.";
+    }
+  } else if (computerMove === "Scissors") {
+    if (yourChoice === "Scissors") {
+      result = "It's a Tie.";
+    } else if (yourChoice === "Rock") {
+      result = "You win.";
+    } else {
+      result = "You lose.";
+    }
+  }
+  if (result === "You win.") {
+    score.wins++;
+  } else if (result === "You lose.") {
+    score.losses++;
+  } else {
+    score.ties++;
+  }
+
+  localStorage.setItem("score", JSON.stringify(score));
+  document.querySelector(".js-players-move").innerHTML = `you
 <img src="game_icons/${yourChoice}-emoji.png" alt="" class="game-icon">
 <img src="game_icons/${computerMove}-emoji.png" alt="" class="game-icon">
 computer`;
-updateScoreElement();
-document.querySelector('.js-result').innerHTML = result;
-
-
+  updateScoreElement();
+  document.querySelector(".js-result").innerHTML = result;
 }
 
-function updateScoreElement(){
-  const scoreElement = document.querySelector('.js-score-element');
+function updateScoreElement() {
+  const scoreElement = document.querySelector(".js-score-element");
   scoreElement.innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
 
-function resetScore(){
+function resetScore() {
   score.wins = 0;
   score.losses = 0;
   score.ties = 0;
 
-  localStorage.removeItem('score');
+  localStorage.removeItem("score");
   updateScoreElement();
-  document.querySelector('.js-result').innerHTML = 'Play The Game';
+  document.querySelector(".js-result").innerHTML = "Play The Game";
+}
 
+let isAutoPlaying = false;
+let intervalId;
 
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const yourChoice = pickComputerMove();
+      createSignals(yourChoice);
+    }, 1000);
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
 }
